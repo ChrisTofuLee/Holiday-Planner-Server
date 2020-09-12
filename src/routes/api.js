0dfd374e22a6411fa2cae1d4a9863d9ef81531df
Selfay <chris.lee90@hotmail.co.uk>
2020-09-12 23:36:28 +0100

    changing api file, updating routes

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,20 +2,23 @@ import axios from 'axios';
 import express from 'express';
 import db from '../models';
 
+require('dotenv').config();
+
+const { GOOGLE_API_KEY } = process.env;
 const apiRouter = express.Router();
 
-const foodURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyC9eRMr-ZCaKj0Ttta4-RQGz0hxnDulTNY';
-const SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Sydney&key=AIzaSyC9eRMr-ZCaKj0Ttta4-RQGz0hxnDulTNY';
-const findPlaceURL = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyC9eRMr-ZCaKj0Ttta4-RQGz0hxnDulTNY';
+const FOOD_URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=${GOOGLE_API_KEY}`;
+const SEARCH_URL = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=restaurants+in+Sydney&key=${GOOGLE_API_KEY}`;
+const FIND_PLACE_URL = `https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Museum%20of%20Contemporary%20Art%20Australia&inputtype=textquery&fields=photos,formatted_address,name,rating,opening_hours,geometry&key=${GOOGLE_API_KEY}`;
 
 const getPlacesFromGoogle = async (req, res) => {
   try {
     // const { searchTerm } = req.body;
 
     const { data } = await axios.get(SEARCH_URL);
-    const IMAGE_URL = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${data.results.photos[0].photo_reference}&key=AIzaSyC9eRMr-ZCaKj0Ttta4-RQGz0hxnDulTNY`;
-    console.log(data, foodURL, findPlaceURL);
-    res.json(data, IMAGE_URL);
+    // const IMAGE_URL = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${data.results.photos[0].photo_reference}&key=${GOOGLE_API_KEY}`;
+    console.log(data, FOOD_URL, FIND_PLACE_URL);
+    res.json(data);
   } catch (error) {
     res.status(500).json({
       error: error.message,
@@ -71,6 +74,7 @@ const savePlaceInDB = async (req, res) => {
 
 apiRouter.get('/cities', getPlacesFromGoogle);
 apiRouter.get('/plans', getAllPlans);
+apiRouter.get('/plan/:id', getPlanById)
 apiRouter.post('/plans', savePlanInDB);
 apiRouter.post('/places', savePlaceInDB);
 apiRouter.delete('/plans/:id', removePlanInDB);
