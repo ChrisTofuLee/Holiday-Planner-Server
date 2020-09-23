089d4346b6b1498a3e8a7ac62460a897514c35d7
Selfay <chris.lee90@hotmail.co.uk>
2020-09-23 16:02:44 +0100

    updated model and route functions

--- src/routes/api.js
+++ src/routes/api.js
@@ -7,7 +7,8 @@ require('dotenv').config();
 const { GOOGLE_API_KEY } = process.env;
 const apiRouter = express.Router();
 
-const GOOGLE_TEXT_SEARCH_URL = 'https://maps.googleapis.com/maps/api/place/textsearch/json';
+const GOOGLE_TEXT_SEARCH_URL =
+  'https://maps.googleapis.com/maps/api/place/textsearch/json';
 const GOOGLE_PLACE_PHOTO = 'https://maps.googleapis.com/maps/api/place/photo';
 
 // const FOOD_URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=${GOOGLE_API_KEY}`;
@@ -18,9 +19,9 @@ const GOOGLE_PLACE_PHOTO = 'https://maps.googleapis.com/maps/api/place/photo';
 
 const getPhotoURL = (googlePlace) => {
   if (
-    googlePlace.photos
-    && googlePlace.photos.length
-    && googlePlace.photos[0].photo_reference
+    googlePlace.photos &&
+    googlePlace.photos.length &&
+    googlePlace.photos[0].photo_reference
   ) {
     return `${GOOGLE_PLACE_PHOTO}?maxwidth=300&photoreference=${googlePlace.photos[0].photo_reference}&key=${GOOGLE_API_KEY}`;
   }
@@ -42,23 +43,27 @@ const getPlaceDetails = async (googlePlace, term) => {
 
 // places details `https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&key=${GOOGLE_API_KEY}` ?limit=10
 
-const transformGooglePlaces = async (googlePlaces = []) => {
+const transformGooglePlaces = async (googlePlaces = [], type) => {
   // const placeOne = await getPlaceDetails(googlePlaces[0], 'reviews');
+
   const data = await Promise.all(
-    googlePlaces.map(async (googlePlace) => ({
-      name: googlePlace.name,
-      address: googlePlace.formatted_address,
-      googlePlacesId: googlePlace.place_id,
-      price: googlePlace.price_level,
-      rating: googlePlace.rating,
-      photo: getPhotoURL(googlePlace),
-      type: googlePlace.types,
-      icon: googlePlace.icon,
-      review: await getPlaceDetails(googlePlace, 'reviews'),
-      googleUrl: await getPlaceDetails(googlePlace, 'url'),
-      siteUrl: await getPlaceDetails(googlePlace, 'website'),
-      openingTimes: await getPlaceDetails(googlePlace, 'weekday_text'),
-    })),
+    googlePlaces.map(async (googlePlace) => {
+      const review = await getPlaceDetails(googlePlace, 'reviews');
+      return {
+        name: googlePlace.name,
+        address: googlePlace.formatted_address,
+        googlePlacesId: googlePlace.place_id,
+        price: googlePlace.price_level,
+        rating: googlePlace.rating,
+        photo: getPhotoURL(googlePlace),
+        type,
+        icon: googlePlace.icon,
+        review,
+        googleUrl: await getPlaceDetails(googlePlace, 'url'),
+        siteUrl: await getPlaceDetails(googlePlace, 'website'),
+        openingTimes: await getPlaceDetails(googlePlace, 'weekday_text'),
+      };
+    }),
   );
   return data;
 };
@@ -83,8 +88,8 @@ const getPlacesFromGoogle = async (req, res) => {
           key: GOOGLE_API_KEY,
         },
       });
-      foodData.results.splice(1, 18);
-      foodResults = await transformGooglePlaces(foodData.results);
+      foodData.results.splice(0, 19);
+      foodResults = await transformGooglePlaces(foodData.results, 'Food');
     }
     if (nightlife) {
       const { data: nightlifeData } = await axios.get(GOOGLE_TEXT_SEARCH_URL, {
@@ -93,7 +98,11 @@ const getPlacesFromGoogle = async (req, res) => {
           key: GOOGLE_API_KEY,
         },
       });
-      nightlifeResults = transformGooglePlaces(nightlifeData.results);
+      nightlifeData.results.splice(0, 19);
+      nightlifeResults = await transformGooglePlaces(
+        nightlifeData.results,
+        'Nightlife',
+      );
     }
     if (activities) {
       const { data: activitiesData } = await axios.get(GOOGLE_TEXT_SEARCH_URL, {
@@ -102,8 +111,13 @@ const getPlacesFromGoogle = async (req, res) => {
           key: GOOGLE_API_KEY,
         },
       });
-      activitiesResults = transformGooglePlaces(activitiesData.results);
+      activitiesData.results.splice(0, 19);
+      activitiesResults = await transformGooglePlaces(
+        activitiesData.results,
+        'Activities',
+      );
     }
+
     res.status(201).json({ foodResults, nightlifeResults, activitiesResults });
   } catch (error) {
     res.status(500).json({
@@ -117,7 +131,7 @@ const getAllPlans = async (req, res) => {
     const {
       user: { id },
     } = req;
-    console.log(req.user);
+    console.log('getallplans', req.user);
     // const allPlans = await db.Plan.find({ userId: id });
     const allPlans = await db.Plan.find({ userId: id }).sort({ createdAt: -1 });
     res.json({ allPlans });
@@ -130,10 +144,10 @@ const getAllPlans = async (req, res) => {
 
 const savePlanInDB = async (req, res) => {
   try {
-    const payload = req.body;
+    const { planName } = req.body;
+    const planTitle = { title: planName };
     const currentUserId = req.user.id;
-    const plan = { ...payload, userId: currentUserId };
-
+    const plan = { ...planTitle, userId: currentUserId };
     const createdPlan = await db.Plan.create(plan);
 
     res.status(201).json({
@@ -149,9 +163,9 @@ const savePlanInDB = async (req, res) => {
 
 const removePlanInDB = async (req, res) => {
   try {
-    const { id } = req.params;
+    const planId = req.params.id;
 
-    await db.Plan.findByIdAndDelete(id);
+    await db.Plan.findByIdAndDelete(planId);
     // await db.Plan.find({});
 
     res.status(201).json({
@@ -216,7 +230,7 @@ const addPlaceInDB = async (req, res) => {
     const plan = await db.Plan.findById(id);
     plan.places.push(payload);
     const placeAdded = await plan.save();
-
+    console.log(id, payload);
     res.status(201).json({
       success: true,
       placeAdded,
@@ -270,10 +284,10 @@ const getPlaceById = async (req, res) => {
 };
 
 apiRouter.post('/cities', getPlacesFromGoogle);
+apiRouter.post('/plans', savePlanInDB);
 apiRouter.get('/plans', getAllPlans);
 apiRouter.get('/plans/:id', getPlanById);
 apiRouter.get('/plans/:id/place/:placeId', getPlaceById);
-apiRouter.post('/plans', savePlanInDB);
 apiRouter.delete('/plans/:id', removePlanInDB);
 apiRouter.put('/plans/:id/addPlace', addPlaceInDB);
 apiRouter.put('/plans/:id/removePlace', removePlaceInDB);
